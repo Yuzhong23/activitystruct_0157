8424c2a331dfda211885ce7ca4328dca4b99eb1a
Yuzhong23 <xixixixifatur@gmail.com>
2024-04-02 10:35:50 +0700

    Membuat cout dari Nomor mahasiswa di dalam int main dan juga memperbaiki eror system tadi

--- activitystruct_0157.cpp
+++ activitystruct_0157.cpp
@@ -4,13 +4,11 @@ using namespace std;
 struct Mahasiswa {
 	string nim;
 	string nama;
-	string nama;
-	string alamat;
-	string nama;
 	string alamat;
 	int umur;
 };
 
 int main() {
-
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
 }
